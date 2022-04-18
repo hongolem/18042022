7a6e99f87afcde63ccda0849e8a36c32af71b523
JanHoly <53258234+hongolem@users.noreply.github.com>
2022-04-18 22:37:44 +0200

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,53 @@
-def on_forever():
-    pass
-basic.forever(on_forever)
+radio.set_group(100)
+
+Lock = ""
+
+def on_button_pressed_a():
+    global Lock
+    if Lock != "A":
+        radio.send_value("answer", 65)
+        Lock = "A"
+        basic.show_string(Lock)
+    elif Lock == "A":
+        basic.show_string(Lock)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    global Lock
+    if Lock != "B":
+        radio.send_value("answer", 66)
+        Lock = "B"
+        basic.show_string(Lock)
+    elif Lock == "B":
+        basic.show_string(Lock)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_pin_pressed_p0():
+    global Lock
+    if Lock != "C":
+        radio.send_value("answer", 67)
+        Lock = "C"
+        basic.show_string(Lock)
+    elif Lock == "C":
+        basic.show_string(Lock)
+input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
+
+def on_pin_pressed_p1():
+    global Lock
+    if Lock != "D":
+        radio.send_value("answer", 68)
+        Lock = "D"
+        basic.show_string(Lock)
+    elif Lock == "D":
+        basic.show_string(Lock)
+input.on_pin_pressed(TouchPin.P1, on_pin_pressed_p1)
+
+def on_pin_pressed_p2():
+    global Lock
+    if Lock != "E":
+        radio.send_value("answer", 69)
+        Lock = "E"
+        basic.show_string(Lock)
+    elif Lock == "E":
+        basic.show_string(Lock)
+input.on_pin_pressed(TouchPin.P2, on_pin_pressed_p2)
--- main.ts
+++ main.ts
@@ -1,3 +1,57 @@
-basic.forever(function on_forever() {
+radio.setGroup(100)
+let Lock = ""
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+    if (Lock != "A") {
+        radio.sendValue("answer", 65)
+        Lock = "A"
+        basic.showString(Lock)
+    } else if (Lock == "A") {
+        basic.showString(Lock)
+    }
+    
+})
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    if (Lock != "B") {
+        radio.sendValue("answer", 66)
+        Lock = "B"
+        basic.showString(Lock)
+    } else if (Lock == "B") {
+        basic.showString(Lock)
+    }
+    
+})
+input.onPinPressed(TouchPin.P0, function on_pin_pressed_p0() {
+    
+    if (Lock != "C") {
+        radio.sendValue("answer", 67)
+        Lock = "C"
+        basic.showString(Lock)
+    } else if (Lock == "C") {
+        basic.showString(Lock)
+    }
+    
+})
+input.onPinPressed(TouchPin.P1, function on_pin_pressed_p1() {
+    
+    if (Lock != "D") {
+        radio.sendValue("answer", 68)
+        Lock = "D"
+        basic.showString(Lock)
+    } else if (Lock == "D") {
+        basic.showString(Lock)
+    }
+    
+})
+input.onPinPressed(TouchPin.P2, function on_pin_pressed_p2() {
+    
+    if (Lock != "E") {
+        radio.sendValue("answer", 69)
+        Lock = "E"
+        basic.showString(Lock)
+    } else if (Lock == "E") {
+        basic.showString(Lock)
+    }
     
 })
