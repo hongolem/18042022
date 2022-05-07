19d8d476e7bacd0ed390f010d5619a04b5f5fb9d
JanHoly <53258234+hongolem@users.noreply.github.com>
2022-05-07 21:23:43 +0200

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,53 +1,70 @@
 radio.set_group(100)
 
 Lock = ""
+Server_running = False
+
+def on_received_string(receivedString):
+    global Server_running
+    basic.show_string(receivedString)
+    Server_running = True
+radio.on_received_string(on_received_string)
 
 def on_button_pressed_a():
     global Lock
-    if Lock != "A":
+    if Lock != "A" and Server_running == True:
         radio.send_value("answer", 65)
         Lock = "A"
         basic.show_string(Lock)
-    elif Lock == "A":
+    elif Lock == "A" and Server_running == True:
         basic.show_string(Lock)
+    elif Server_running == False:
+        basic.show_string("Hlasování nezačalo!")
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_b():
     global Lock
-    if Lock != "B":
+    if Lock != "B" and Server_running == True:
         radio.send_value("answer", 66)
         Lock = "B"
         basic.show_string(Lock)
-    elif Lock == "B":
+    elif Lock == "B" and Server_running == True:
         basic.show_string(Lock)
+    elif Server_running == False:
+        basic.show_string("Hlasování nezačalo!")
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_pin_released_p0():
     global Lock
-    if Lock != "C":
+    if Lock != "C" and Server_running == True:
         radio.send_value("answer", 67)
         Lock = "C"
         basic.show_string(Lock)
-    elif Lock == "C":
+    elif Lock == "C" and Server_running == True:
         basic.show_string(Lock)
+    elif Server_running == False:
+        basic.show_string("Hlasování nezačalo!")
 input.on_pin_released(TouchPin.P0, on_pin_released_p0)
 
 def on_pin_released_p1():
     global Lock
-    if Lock != "D":
+    if Lock != "D" and Server_running == True:
         radio.send_value("answer", 68)
         Lock = "D"
         basic.show_string(Lock)
-    elif Lock == "D":
+    elif Lock == "D" and Server_running == True:
         basic.show_string(Lock)
+    elif Server_running == False:
+        basic.show_string("Hlasování nezačalo!")
 input.on_pin_released(TouchPin.P1, on_pin_released_p1)
 
 def on_pin_released_p2():
     global Lock
-    if Lock != "E":
+    if Lock != "E" and Server_running == True:
         radio.send_value("answer", 69)
         Lock = "E"
         basic.show_string(Lock)
-    elif Lock == "E":
+    elif Lock == "E" and Server_running == True:
         basic.show_string(Lock)
+    elif Server_running == False:
+        basic.show_string("Hlasování nezačalo!")
 input.on_pin_released(TouchPin.P2, on_pin_released_p2)
--- main.ts
+++ main.ts
@@ -1,57 +1,73 @@
 radio.setGroup(100)
 let Lock = ""
+let Server_running = false
+radio.onReceivedString(function on_received_string(receivedString: string) {
+    
+    basic.showString(receivedString)
+    Server_running = true
+})
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
     
-    if (Lock != "A") {
+    if (Lock != "A" && Server_running == true) {
         radio.sendValue("answer", 65)
         Lock = "A"
         basic.showString(Lock)
-    } else if (Lock == "A") {
+    } else if (Lock == "A" && Server_running == true) {
         basic.showString(Lock)
+    } else if (Server_running == false) {
+        basic.showString("Hlasování nezačalo!")
     }
     
 })
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     
-    if (Lock != "B") {
+    if (Lock != "B" && Server_running == true) {
         radio.sendValue("answer", 66)
         Lock = "B"
         basic.showString(Lock)
-    } else if (Lock == "B") {
+    } else if (Lock == "B" && Server_running == true) {
         basic.showString(Lock)
+    } else if (Server_running == false) {
+        basic.showString("Hlasování nezačalo!")
     }
     
 })
 input.onPinReleased(TouchPin.P0, function on_pin_released_p0() {
     
-    if (Lock != "C") {
+    if (Lock != "C" && Server_running == true) {
         radio.sendValue("answer", 67)
         Lock = "C"
         basic.showString(Lock)
-    } else if (Lock == "C") {
+    } else if (Lock == "C" && Server_running == true) {
         basic.showString(Lock)
+    } else if (Server_running == false) {
+        basic.showString("Hlasování nezačalo!")
     }
     
 })
 input.onPinReleased(TouchPin.P1, function on_pin_released_p1() {
     
-    if (Lock != "D") {
+    if (Lock != "D" && Server_running == true) {
         radio.sendValue("answer", 68)
         Lock = "D"
         basic.showString(Lock)
-    } else if (Lock == "D") {
+    } else if (Lock == "D" && Server_running == true) {
         basic.showString(Lock)
+    } else if (Server_running == false) {
+        basic.showString("Hlasování nezačalo!")
     }
     
 })
 input.onPinReleased(TouchPin.P2, function on_pin_released_p2() {
     
-    if (Lock != "E") {
+    if (Lock != "E" && Server_running == true) {
         radio.sendValue("answer", 69)
         Lock = "E"
         basic.showString(Lock)
-    } else if (Lock == "E") {
+    } else if (Lock == "E" && Server_running == true) {
         basic.showString(Lock)
+    } else if (Server_running == false) {
+        basic.showString("Hlasování nezačalo!")
     }
     
 })
